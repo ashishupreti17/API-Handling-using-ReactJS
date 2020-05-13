da27d7b95c48351e70ced5ddb2499397585b9c57
Ashish Upreti <server@server.com>
2020-05-13 20:27:59 +0530

    intial Commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import {Container, Row, Col} from 'reactstrap';
+import './App.css';
+import Axios from 'axios';
+import MyCard from './MyCard';
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  
+  const [details,setDetails] = useState({})
+  
+  const fetchDetails = async () => {
+    const {data} = await Axios.get("https://randomuser.me/api/")
+    console.log("RESPONSE : ",data);
+
+    const details = data.results[0];
+    setDetails(details);
+
+  }
+
+  useEffect(() => {
+    fetchDetails();
+  },[])
+    return (
+      <Container fluid className="p-4 bg-primary App">
+        <Row>
+          <Col md={4} className="offset-md-4 mt-4">
+          <MyCard details={details} />
+          </Col>
+        </Row>
+
+
+      </Container>
+      );
+}
+
+export default App;
+ 
